d737352f9009455492ba5a39adeaf8859216fb0d
tomaszsmyczynski <04ts04@gmail.com>
2020-11-17 18:47:35 +0100

    part2 cleanup

--- smb/HelloWorld/app/src/main/java/com/example/helloworld/ListActivity.kt
+++ smb/HelloWorld/app/src/main/java/com/example/helloworld/ListActivity.kt
@@ -10,11 +10,9 @@ import com.example.helloworld.databinding.ActivityListBinding
 
 class ListActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityListBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityListBinding.inflate(layoutInflater)
+        val binding = ActivityListBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.rv1.layoutManager = LinearLayoutManager(baseContext)
         binding.rv1.addItemDecoration(
