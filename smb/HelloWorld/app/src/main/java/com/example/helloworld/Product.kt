89d715311789648661612ea06efe292b32311fa3
tomaszsmyczynski <04ts04@gmail.com>
2020-11-18 12:29:36 +0100

    part3

--- smb/HelloWorld/app/src/main/java/com/example/helloworld/Product.kt
+++ smb/HelloWorld/app/src/main/java/com/example/helloworld/Product.kt
@@ -1,5 +1,6 @@
 package com.example.helloworld
 
+import android.content.ContentValues
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
@@ -7,4 +8,16 @@ import androidx.room.PrimaryKey
 data class Product(var name: String, var price: Double, var amount: Int, var isBought: Boolean) {
     @PrimaryKey(autoGenerate = true)
     var id: Long = 0
+
+    companion object {
+        fun fromContentValues(values: ContentValues?): Product {
+            values?.let{
+                return Product(
+                        values.getAsString("name"),
+                        values.getAsDouble("price"),
+                        values.getAsInteger("amount"),
+                        values.getAsBoolean("isBought"))
+            } ?: throw IllegalArgumentException()
+        }
+    }
 }
--- smb/HelloWorld/app/src/main/java/com/example/helloworld/ProductDao.kt
+++ smb/HelloWorld/app/src/main/java/com/example/helloworld/ProductDao.kt
@@ -1,5 +1,6 @@
 package com.example.helloworld
 
+import android.database.Cursor
 import androidx.lifecycle.LiveData
 import androidx.room.*
 
@@ -9,14 +10,20 @@ interface ProductDao {
     @Query("SELECT * FROM product_table")
     fun getProducts(): LiveData<List<Product>>
 
+    @Query("SELECT * FROM product_table")
+    fun findAll(): Cursor
+
     @Insert
-    fun insert(product: Product)
+    fun insert(product: Product) : Long
 
     @Update
-    fun update(product: Product)
+    fun update(product: Product) : Int
 
     @Delete
     fun delete(product: Product)
+
+    @Query("DELETE FROM product_table WHERE id = :productId")
+    fun delete(productId: Long) : Int
 
     @Query("DELETE FROM product_table")
     fun clear()
